ac6cdea8baff70c7252a601662e2d734f3f86621
unknown <chandramaulishastri@gmail.com>
2020-11-30 13:55:04 +0530

    Console starting added

--- data.py
+++ data.py
@@ -1,2 +1,36 @@
-#importing modules & libs
-import mysql.connector as sqt
+# importing modules & libs
+import mysql.connector
+import colorama
+import sys
+import os
+import time
+
+# Login
+user = input("Enter user => ")
+password = input("Enter password => ")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+mydb = mysql.connector.connect(host='localhost', user= user, passwd= password)
+state = mydb.is_connected()
+cursor = mydb.cursor()
+
+# Starting
+if state == True:
+    print("Successfully connected to DB ;) ")
+    run = input("Start DB console (Y/N)? => ")
+
+    if run == "Y":
+        running = True
+        print("Starting the console...")
+
+    elif run == "N":
+        running = False
+        print("Exiting the console...")
+        time.sleep(3)
+        exit
+else:
+    print("Please check user and password :/ ")
+
+
+
+while running:
+    break
