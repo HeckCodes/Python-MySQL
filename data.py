a4b2f2cb9849145afb5f0357869ccdfdc49fcdc4
unknown <chandramaulishastri@gmail.com>
2020-11-30 16:27:19 +0530

    Formatted Starting

--- data.py
+++ data.py
@@ -1,3 +1,5 @@
+
+
 # importing modules & libs
 import mysql.connector
 import colorama
@@ -5,32 +7,42 @@ import sys
 import os
 import time
 
+# Functions
+
+
 # Login
 user = input("Enter user => ")
 password = input("Enter password => ")
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-mydb = mysql.connector.connect(host='localhost', user= user, passwd= password)
-state = mydb.is_connected()
-cursor = mydb.cursor()
+try:
+    mydb = mysql.connector.connect(host='localhost', user= user, passwd= password)
+    state = mydb.is_connected()
+    cursor = mydb.cursor()
 
-# Starting
-if state == True:
-    print("Successfully connected to DB ;) ")
-    run = input("Start DB console (Y/N)? => ")
+    # Starting
+    if state == True:
+        os.system('cls')
+        print("Checking for user : \033[1m <{}>\033[0m".format(user))
+        print("Injecting password : \033[32;1m <{}>\033[0m".format("*"*len(password)))
+        print("--------------------------------")
+        print("Successfully connected to DB ;) ")
+        run = input("Start DB console (Y/N)? => ")
 
-    if run == "Y":
-        running = True
-        print("Starting the console...")
+        if run == "Y":
+            running = True
+            print("Starting the console...")
 
-    elif run == "N":
-        running = False
-        print("Exiting the console...")
-        time.sleep(3)
-        exit
-else:
-    print("Please check user and password :/ ")
+        elif run == "N":
+            running = False
+            print("Exiting the console...")
+            time.sleep(3)
+            exit
 
-
+except mysql.connector.errors.ProgrammingError:
+    os.system("cls")
+    running = False
+    print("\033[91mPlease check user and password :/\033[0m")
+    time.sleep(4)
+    exit
 
 while running:
     break
